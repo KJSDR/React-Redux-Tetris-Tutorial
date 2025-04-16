aa9f1d8873ffb1f0b72a052eef47500fd73826ab
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-04-15 20:40:55 -0700

    added initial controls

---
+++ src/components/Controls.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Controls() {
+  return (
+    <div className="controls">
+      <button className="control-button" onClick={() => {}}>Left</button>
+      <button className="control-button" onClick={() => {}}>Right</button>
+      <button className="control-button" onClick={() => {}}>Rotate</button>
+      <button className="control-button" onClick={() => {}}>Down</button>
+    </div>
+  );
+}
