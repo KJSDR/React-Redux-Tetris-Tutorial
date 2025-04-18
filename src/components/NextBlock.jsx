fa24d7249d85dc2d36d7458e8826368e7a76679e
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-04-18 09:24:46 -0700

    added connection for next block

--- src/components/NextBlock.jsx
+++ src/components/NextBlock.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import GridSquare from './GridSquare';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils';
 
-// Draws the "next" block view showing the next block to drop
 export default function NextBlock() {
-  const block = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  const nextShape = useSelector((state) => state.nextShape);
+  const block = shapes[nextShape][0];
 
   const grid = block.map((rowArray, row) =>
-    rowArray.map((square, col) => (
-      <GridSquare key={`${row}${col}`} color={square} />
-    ))
+    rowArray.map((square, col) => {
+      const color = square ? nextShape : 0;
+      return <GridSquare key={`${row}${col}`} color={color} />;
+    })
   );
 
   return <div className="next-block">{grid}</div>;
