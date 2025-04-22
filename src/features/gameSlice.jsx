32ce5a262149a296b0f98c54cda94af3507ce476
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-04-21 23:05:11 -0700

    move left and right

--- src/features/gameSlice.jsx
+++ src/features/gameSlice.jsx
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { defaultState, nextRotation, canMoveTo } from '../utils';
+import {
+  defaultState,
+  nextRotation,
+  canMoveTo,
+  addBlockToGrid,
+  checkRows,
+  randomShape,
+} from '../utils';
 
 export const gameSlice = createSlice({
   name: 'game',
@@ -7,9 +14,49 @@ export const gameSlice = createSlice({
   reducers: {
     pause: () => {},
     resume: () => {},
-    moveLeft: () => {},
-    moveRight: () => {},
-    moveDown: () => {},
+    moveLeft: (state) => {
+      const { shape, grid, x, y, rotation } = state;
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        state.x = x - 1;
+      }
+      return state;
+    },
+    moveRight: (state) => {
+      const { shape, grid, x, y, rotation } = state;
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        state.x = x + 1;
+      }
+      return state;
+    },
+    moveDown: (state) => {
+      const { x, y, shape, grid, rotation, nextShape } = state;
+      const maybeY = y + 1;
+
+      if (canMoveTo(shape, grid, x, maybeY, rotation)) {
+        state.y = maybeY;
+        return state;
+      }
+
+      // Can't move down: lock block into grid
+      const newGrid = addBlockToGrid(shape, grid, x, y, rotation);
+
+      state.grid = newGrid;
+      state.x = 3;
+      state.y = -4;
+      state.rotation = 0;
+      state.shape = nextShape;
+      state.nextShape = randomShape();
+
+      // Game over check
+      if (!canMoveTo(state.shape, newGrid, state.x, state.y, state.rotation)) {
+        state.shape = 0;
+        state.gameOver = true;
+        return state;
+      }
+
+      state.score += checkRows(newGrid);
+      return state;
+    },
     rotate: (state) => {
       const { shape, grid, x, y, rotation } = state;
       const newRotation = nextRotation(shape, rotation);
