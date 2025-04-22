aae288ff5ae7997d808c2a06808f112341d0b199
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-04-21 22:39:47 -0700

    added connection for score board

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { pause, resume, restart } from '../features/gameSlice';
 
 export default function ScoreBoard() {
+  const dispatch = useDispatch();
+  const { score, isRunning, gameOver, level } = useSelector((state) => state);
+
   return (
     <div className="score-board">
-      <div>Score: 0</div>
-      <div>Level: 1</div>
-      <button className="score-board-button" onClick={(e) => {
-        // ...
-      }}>Play</button>
-      <button className="score-board-button" onClick={(e) => {
-        // ...
-      }}>Restart</button>
+      <div>Score: {score}</div>
+      <div>Level: {level}</div>
+      
+      <button
+        className="score-board-button"
+        onClick={() => {
+          if (gameOver) return;
+          if (isRunning) {
+            dispatch(pause());
+          } else {
+            dispatch(resume());
+          }
+        }}
+      >
+        {isRunning ? 'Pause' : 'Play'}
+      </button>
+
+      <button
+        className="score-board-button"
+        onClick={() => dispatch(restart())}
+      >
+        Restart
+      </button>
     </div>
   );
 }
